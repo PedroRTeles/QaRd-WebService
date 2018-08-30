04eb56552160172bb6cfc7d8b573419b0f21cb65
Pedro Teles <pedroateles@gmail.com>
2018-08-30 17:01:13 -0300

    Fix indentation

--- DAO/BaseDAO.php
+++ DAO/BaseDAO.php
@@ -1,25 +1,25 @@
 <?php
 
-  class BaseDAO
-  {
-      private $config;
-      private $connection = null;
+class BaseDAO
+{
+	private $config;
+	private $connection = null;
 
-      public function __construct()
-      {
-          include("config.php"); //Create config.php in the same folder
-          $this->config = $config;
-          $this->connect();
-      }
+	public function __construct()
+	{
+		include("config.php"); //Create config.php in the same folder
+		$this->config = $config;
+		$this->connect();
+	}
 
-      public function getConnection()
-      {
-        return $this->connection;
-      }
+	public function getConnection()
+	{
+		return $this->connection;
+	}
 
-      private function connect()
-      {
-        $this->connection = new mysqli($this->config["server"], $this->config["username"], $this->config["password"], $this->config["dbName"]);
-      }
-  }
+	private function connect()
+	{
+		$this->connection = new mysqli($this->config["server"], $this->config["username"], $this->config["password"], $this->config["dbName"]);
+	}
+}
 ?>
--- Model/Checkin.php
+++ Model/Checkin.php
@@ -1,22 +1,22 @@
 <?php
-  class Checkin
-  {
-    public $idCheckin;
-    public $idEmployee;
-    public $hour;
-    public $date;
-    public $type;
-    public $authCode;
+class Checkin
+{
+	public $idCheckin;
+	public $idEmployee;
+	public $hour;
+	public $date;
+	public $type;
+	public $authCode;
 
-    function __construct()
-    {
-      $this->idCheckin = 0;
-      $this->idEmployee = 0;
-      $this->hour = "";
-      $this->date = "";
-      $this->type = 0;
-      $this->authCode = "";
-    }
-  }
+	function __construct()
+	{
+		$this->idCheckin = 0;
+		$this->idEmployee = 0;
+		$this->hour = "";
+		$this->date = "";
+		$this->type = 0;
+		$this->authCode = "";
+	}
+}
 
 ?>
